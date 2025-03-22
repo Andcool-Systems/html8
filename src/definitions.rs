695c1493f5db355e2980aa870cba6150c4dc02d2
AndcoolSystems <drolll2007@gmail.com>
2025-03-22 17:08:04 +0300

    added duplicate arg check

--- src/definitions.rs
+++ src/definitions.rs
@@ -1,4 +1,4 @@
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 
 use crate::code_tree::types::{ArgStruct, AssignEnum, CallArgStruct};
 use crate::math::errors::DefinitionNotFound;
@@ -19,6 +19,25 @@ pub enum Defined {
 pub fn start_def_check(tree: &mut NodeType) {
     let mut defined: HashMap<String, Defined> = HashMap::new();
     check(tree, &mut defined);
+}
+
+fn find_duplicate<T: Eq + std::hash::Hash + Clone>(arr: &[T]) -> Option<T> {
+    let mut set = HashSet::new();
+    for item in arr {
+        if !set.insert(item) {
+            return Some(item.clone());
+        }
+    }
+    None
+}
+
+fn check_duplicate_def(args: Vec<String>, calling_name: String) {
+    if let Some(duplicate) = find_duplicate(&args) {
+        panic!(
+            "Found duplicate argument `{}` in function: {}",
+            duplicate, calling_name
+        );
+    }
 }
 
 fn check(tree: &mut NodeType, defined: &mut HashMap<String, Defined>) {
@@ -50,6 +69,11 @@ fn check(tree: &mut NodeType, defined: &mut HashMap<String, Defined>) {
                     defined.insert(arg.name.clone(), var);
                 });
 
+                check_duplicate_def(
+                    fds.args.iter().map(|a| a.name.clone()).collect(),
+                    fds.name.clone(),
+                );
+
                 let scope = defined.clone();
                 fds.children
                     .iter_mut()
@@ -75,7 +99,9 @@ fn check(tree: &mut NodeType, defined: &mut HashMap<String, Defined>) {
                             });
                     }
                     AssignEnum::Call(node_type) => match *node_type.clone() {
-                        NodeType::CALL(call_struct) => {
+                        NodeType::CALL(mut call_struct) => {
+                            check_fn_call(defined, &mut call_struct);
+
                             defined.get(&call_struct.calling_name).expect(&format!(
                                 "Function `{}` not defined",
                                 call_struct.calling_name
@@ -93,55 +119,7 @@ fn check(tree: &mut NodeType, defined: &mut HashMap<String, Defined>) {
                 defined.insert(vds.name.clone(), Defined::Variable(vds.clone()));
             }
         },
-        NodeType::CALL(call_struct) => {
-            // Get calling variable by name
-            let entry = defined.get(&call_struct.calling_name);
-
-            entry.is_none().then(|| {
-                panic!(
-                    "Cannot call undefined function: {}",
-                    call_struct.calling_name
-                );
-            });
-
-            if let Some(Defined::Variable(vds)) = entry {
-                panic!("Cannot call variable as function: {}", vds.name);
-            }
-
-            if let Some(Defined::Function(f)) = entry {
-                f.args.clone().into_iter().for_each(|arg: ArgStruct| {
-                    (!call_struct
-                        .args
-                        .iter()
-                        .any(|a: &CallArgStruct| a.name == arg.name))
-                    .then(|| {
-                        panic!(
-                            "Argument `{}` in function `{}` call is required",
-                            arg.name, f.name
-                        );
-                    });
-                });
-            }
-
-            // Check call args
-            call_struct.args.iter().for_each(|arg: &CallArgStruct| {
-                if let Some(argv) = &arg.value {
-                    argv.check_def(defined)
-                        .unwrap_or_else(|e: DefinitionNotFound| {
-                            panic!("Variable `{}` not defined", e.var_name)
-                        });
-                }
-
-                if let Some(Defined::Function(f)) = entry {
-                    (!f.args.iter().any(|a: &ArgStruct| a.name == arg.name)).then(|| {
-                        panic!(
-                            "Unexpected argument `{}` for function `{}`",
-                            arg.name, f.name
-                        );
-                    });
-                }
-            });
-        }
+        NodeType::CALL(call_struct) => check_fn_call(defined, call_struct),
         NodeType::ASSIGN(ref mut call_arg_struct) => {
             match defined.get(&call_arg_struct.name) {
                 Some(Defined::Function(_)) => {
@@ -167,4 +145,61 @@ fn check(tree: &mut NodeType, defined: &mut HashMap<String, Defined>) {
             }
         }
     }
+}
+
+fn check_fn_call(
+    defined: &mut HashMap<String, Defined>,
+    call_struct: &mut crate::code_tree::types::CallStruct,
+) {
+    let entry = defined.get(&call_struct.calling_name);
+
+    entry.is_none().then(|| {
+        panic!(
+            "Cannot call undefined function: {}",
+            call_struct.calling_name
+        );
+    });
+
+    if let Some(Defined::Variable(vds)) = entry {
+        panic!("Cannot call variable as function: {}", vds.name);
+    }
+
+    if let Some(Defined::Function(f)) = entry {
+        f.args.clone().into_iter().for_each(|arg: ArgStruct| {
+            (!call_struct
+                .args
+                .iter()
+                .any(|a: &CallArgStruct| a.name == arg.name))
+            .then(|| {
+                panic!(
+                    "Argument `{}` in function `{}` call is required",
+                    arg.name, f.name
+                );
+            });
+        });
+
+        check_duplicate_def(
+            call_struct.args.iter().map(|a| a.name.clone()).collect(),
+            f.name.clone(),
+        );
+    }
+
+    // Check call args
+    call_struct.args.iter().for_each(|arg: &CallArgStruct| {
+        if let Some(argv) = &arg.value {
+            argv.check_def(defined)
+                .unwrap_or_else(|e: DefinitionNotFound| {
+                    panic!("Variable `{}` not defined", e.var_name)
+                });
+        }
+
+        if let Some(Defined::Function(f)) = entry {
+            (!f.args.iter().any(|a: &ArgStruct| a.name == arg.name)).then(|| {
+                panic!(
+                    "Unexpected argument `{}` for function `{}`",
+                    arg.name, f.name
+                );
+            });
+        }
+    });
 }
